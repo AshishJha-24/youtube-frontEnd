e44fe73d17f6ccc74fc00f0f021e2ca22f299e08
AshishJha-24 <aj230104@gmail.com>
2024-08-13 16:16:53 +0530

    error in tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,16 +5,17 @@ import { useSelector } from 'react-redux';
 const Tweet = (Comment)=>{
     return(props)=>{
          const data= useOutletContext();
+         console.log(data);
+         console.log(process.env.API_ENTRYPOINT)
          tweetfetchApi=process.env.API_ENTRYPOINT+"/api/v1/tweets/user/"+data.userId;
+         console.log(tweetfetchApi)
          tweetPostApi=process.env.API_ENTRYPOINT+"/api/v1/tweets";
          toggletweetLike=process.env.API_ENTRYPOINT+"/api/v1/likes/toggle/t/";
          tweetUpdateApi=process.env.API_ENTRYPOINT+"/api/v1/tweets/";
          tweetDeleteApi=process.env.API_ENTRYPOINT+"/api/v1/tweets/";
 
          const Currentuser=useSelector((state)=>state.user)
-         console.log(Currentuser)
-
-         console.log(data);
+   
          let visibiltyoftweetBox="none"
          if(Currentuser?.user?._id===data.userId){
             console.log(Currentuser?.user?._id +" "+data.userId )
